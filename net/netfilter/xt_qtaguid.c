ccdbaa50eb80827cc61d5ecd09da698a4287eb6b
Mike B <you@example.com>
2019-08-06 18:29:27 +0100

    Config: Qemu, select features closer to device's. Net: Check to include IPv6 function.

--- net/netfilter/xt_qtaguid.c
+++ net/netfilter/xt_qtaguid.c
@@ -1600,12 +1600,15 @@ static struct sock *qtaguid_find_sk(const struct sk_buff *skb,
 		return NULL;
 
 	switch (par->family) {
+#if IS_ENABLED(CONFIG_IP6_NF_IPTABLES)
 	case NFPROTO_IPV6:
 		sk = xt_socket_get6_sk(skb, par);
 		break;
+#else
 	case NFPROTO_IPV4:
 		sk = xt_socket_get4_sk(skb, par);
 		break;
+#endif
 	default:
 		return NULL;
 	}
