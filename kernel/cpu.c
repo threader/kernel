a8a6513598f6a64aecc0e29953538f58333f9344
threader <michael.heltne@gmail.com>
2020-05-15 07:21:06 +0100

    kernel: __unregister_cpu_notifier never changed in later kernels before being deprecated, reverting
    (cherry picked from commit 5f06213d79ef8e2ac842a516d9d81cee162828f1)

--- kernel/cpu.c
+++ kernel/cpu.c
@@ -1,4 +1,4 @@
-/* CPU control.
+ /* CPU control.
  * (C) 2001, 2002, 2003, 2004 Rusty Russell
  *
  * This code is licenced under the GPL.
@@ -214,7 +214,13 @@ void __ref unregister_cpu_notifier(struct notifier_block *nb)
 }
 EXPORT_SYMBOL(unregister_cpu_notifier);
 
+void __ref __unregister_cpu_notifier(struct notifier_block *nb)
+{
+	raw_notifier_chain_unregister(&cpu_chain, nb);
+}
+EXPORT_SYMBOL(__unregister_cpu_notifier);
 
+#ifdef CONFIG_HOTPLUG_CPU
 /**
  * clear_tasks_mm_cpumask - Safely clear tasks' mm_cpumask for a CPU
  * @cpu: a CPU id
